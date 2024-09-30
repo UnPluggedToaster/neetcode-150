12cc1b7f83b79d5a4807dcd2ca4e560601d6861f
Hassan Allam <hallam.allam96@outlook.com>
2024-09-30 18:42:12 -0400

    Rename folder

--- cole/day01_contains-duplicate/contains-duplicate.py
+++
@@ -1,5 +0,0 @@
-# beats 99.94% >:D
-
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
--- cole/day29_group-anagrams/group-anagrams.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        from collections import defaultdict
-
-        counts = defaultdict(list)
-        for s in strs:
-            counts[tuple(sorted(s))].append(s)
-        return list(counts.values())
